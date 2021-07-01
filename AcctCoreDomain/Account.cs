dfc96322bcbfd0996f89e571892e1d6748ae0b94
hardoverton <john.hard@overtonventures.com>
2021-07-01 15:01:49 -0500

    Add files via upload

---
+++ AcctCoreDomain/Account.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AcctCoreDomain
+{
+	public class Account : BaseModel
+	{
+		#region Fields
+		private int acctID;
+		private string acctName;
+		private int acctNumber;
+		private int acctType;
+		private double openBalance;
+		private double acctBalance;
+		private string acctDescription;
+		private int isSubOf;
+		private int inGroup;
+		private ObservableCollection<EntryLine> entryLines;
+		#endregion
+
+		#region Properties
+		//public int ID
+		//{
+		//	get { return acctID; }
+		//	set { acctID = value; }
+		//}
+		[Key]
+		[DatabaseGenerated(DatabaseGeneratedOption.None)]
+		public int AccountNumber
+		{
+			get { return acctNumber; }
+			set
+			{
+				acctNumber = value;
+				OnPropertyChanged();
+			}
+		}
+		public string AccountName
+		{
+			get { return acctName; }
+			set
+			{
+				acctName = value;
+				OnPropertyChanged();
+			}
+		}
+		public int AccountType
+		{
+			get { return acctType; }
+			set
+			{
+				acctType = value;
+				OnPropertyChanged();
+			}
+		}
+		public ObservableCollection<EntryLine> EntryLines
+		{
+			get { return entryLines; }
+			set 
+			{
+				entryLines = value;
+				OnPropertyChanged();
+			}
+		}
+		public double OpeningBalance
+		{
+			get { return openBalance; }
+			set
+			{
+				openBalance = value;
+				OnPropertyChanged();
+			}
+		}
+		public double Balance
+		{
+			get { return acctBalance; }
+			set
+			{
+				acctBalance = value;
+				OnPropertyChanged();
+			}
+		}
+		public string Description
+		{
+			get { return acctDescription; }
+			set
+			{
+				acctDescription = value;
+				OnPropertyChanged();
+			}
+		}
+		public int IsSubOf
+		{
+			get { return isSubOf; }
+			set
+			{
+				isSubOf = value;
+				OnPropertyChanged();
+			}
+		}
+		public int InGroup
+		{
+			get { return inGroup; }
+			set
+			{
+				inGroup = value;
+				OnPropertyChanged();
+			}
+		}
+		#endregion
+
+		#region Constructors
+		public Account()
+		{
+			//acctID = 0;
+			acctName = "";
+			acctNumber = 0;
+			acctType = 0;
+			acctBalance = 0.0;
+			acctDescription = "";
+			isSubOf = 0;
+		}
+		public Account(int num, string nam, int typ)
+		{
+			//acctID = 0;
+			acctName = nam;
+			acctNumber = num;
+			acctType = typ;
+			//acctBalance = bal;
+			//acctDescription = desc;
+			//isSubOf = sub;
+		}
+		#endregion
+
+		#region Methods
+		public int GetNewID()
+		{
+			return 0;
+		}
+		#endregion
+
+
+	}
+
+	public class BaseAcct : BaseModel
+	{
+		private int acctNumber;
+		private string acctName;
+
+		public int Acct
+		{
+			get { return acctNumber; }
+			set { acctNumber = value; }
+		}
+		public string Name
+		{
+			get { return acctName; }
+			set { acctName = value; }
+		}
+
+		public BaseAcct(int acct, string nam)
+		{
+			acctNumber = acct;
+			acctName = nam;
+		}
+	}
+}
---
+++ AcctCoreDomain/Entry.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcctCoreDomain
+{
+	public class Entry : BaseModel
+	{
+		#region Fields
+		private int entryID;
+		private bool genJournal;
+		private DateTime eDate;
+		private List<EntryLine> entries;
+		#endregion
+
+		#region Properties
+		public int EntryID
+		{
+			get { return entryID; }
+			set 
+			{ 
+				entryID = value;
+				OnPropertyChanged();
+			}
+		}
+		public bool GeneralJournal
+		{
+			get { return genJournal; }
+			set 
+			{ 
+				genJournal = value;
+				OnPropertyChanged();
+			}
+		}
+		public DateTime Date
+		{
+			get { return eDate; }
+			set 
+			{ 
+				eDate = value;
+				OnPropertyChanged();
+			}
+		}
+		public List<EntryLine> Lines
+		{
+			get { return entries; }
+			set 
+			{ 
+				entries = value;
+				OnPropertyChanged();
+			}
+		}
+
+		#endregion
+
+		#region Constructors
+		public Entry()
+		{
+			entryID = 0;
+			eDate = DateTime.MinValue;
+			genJournal = false;
+			//Lines = new List<EntryLine>();
+		}
+		public Entry(int entID, DateTime dt, bool gj=false, List<EntryLine> el = null)
+		{
+			entryID = entID;
+			eDate = dt;
+			GeneralJournal = gj;
+			//Lines = new List<EntryLine>();
+			//Lines = el;		// need to copy or byValue
+		}
+		#endregion
+
+		#region Methods
+		private int GetNextEntryID()
+		{
+			// Get From Database or maintain internal list
+			return 0;
+		}
+		public bool IsBalanced()
+		{
+			double balance = 0.0;
+			foreach (EntryLine ent in Lines)
+			{
+				switch (ent.Type)
+				{
+					case (int)EntryType.DR:
+						balance += (double)ent.Deposit;
+						break;
+					case (int)EntryType.CR:
+						balance -= (double)ent.Payment;
+						break;
+				}
+			}
+			return balance == 0.0 ? true : false;
+		}
+		public bool HasAccount(int acctID)
+		{
+			foreach (EntryLine el in Lines)
+			{
+				if (el.AcctNum == acctID)
+					return true;
+			}
+			return false;
+		}
+		public void AddTags(string tags)
+		{
+
+		}
+		public void AddTags(int tag)
+		{
+
+		}
+		#endregion
+
+
+	}
+
+	public class FullEntry : BaseModel
+	{
+
+		private Entry entry;
+		private ObservableCollection<EntryLine> lines;
+
+		public Entry Entry
+		{
+			get { return entry; }
+			set
+			{
+				entry = value;
+				OnPropertyChanged(nameof(Entry));
+			}
+		}
+		public ObservableCollection<EntryLine> EntryLines
+		{
+			get { return lines; }
+			set
+			{
+				lines = value;
+				OnPropertyChanged(nameof(EntryLines));
+			}
+		}
+
+		public FullEntry()
+		{ }
+
+		public FullEntry(Entry ent = null, ObservableCollection<EntryLine> ln = null)
+		{
+			entry = ent;
+			lines = ln;
+		}
+	}
+}
+
+
